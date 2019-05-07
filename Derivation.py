2b7e9c92e09517d1a34092b8e389d6e846a0af7e
Corey Spohn <cas584@cornell.edu>
2019-05-07 16:20:04 -0400

    Init
    Publishing schtuff

---
+++ Derivation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 15:47:24 2019
+
+@author: corey
+"""
+
+from sympy import *
+init_printing()
+
+t1, t2, x, l1, l2, m1, m2 = symbols('theta_1 theta_2 x l_1 l_2 m_1 m_2')
