66901b752ffec38b9219c55140b6bb0bffa12e1c
Corey Spohn <cas584@cornell.edu>
2019-05-09 16:38:44 -0400

    Movement stuff

--- animations.py
+++ animations.py
@@ -21,8 +21,19 @@ class CartDemo(Scene):
         m2radius = 0.25
         
         # Lengths, should also add angle
-        l1 = 1
-        l2 = 1
+        l1 = 2
+        l2 = 2
+        
+        # Angles (rad)
+        t1 = 30 *np.pi/180
+        t2 = 30 *np.pi/180
+        
+        # Unit vectors
+        er1 = -np.sin(t1)*RIGHT+np.cos(t1)*UP
+        et1 = -np.cos(t1)*RIGHT-np.sin(t1)*UP
+        
+        er2 = np.sin(t2)*RIGHT+np.cos(t2)*UP
+        et2 = np.cos(t2)*RIGHT-np.sin(t2)*UP
         
         # Cart properties
         r_M_O = [0, -1] # Position of cart wrt the origin
@@ -41,8 +52,8 @@ class CartDemo(Scene):
         r_C2_O = [r_M_O[0]+0.25*d_M[1], r_M_O[1]+0.5*d_M[0], 0]
         
         # Position of masses relative to their hinges
-        r_m1_C1 = [-0.5, 2, 0] 
-        r_m2_C2 = [0.5, 2, 0]
+        r_m1_C1 = l1*er1
+        r_m2_C2 = l2*er2
         
         # Position of masses relative to origin
         r_m1_O = [r_m1_C1[0]+r_C1_O[0], r_m1_C1[1]+r_C1_O[1], 0]
@@ -53,30 +64,46 @@ class CartDemo(Scene):
         BLUE = '#4DA4B5'
         PURPLE = '#B696D2'
         MAROON = '#761648'
+        GREY_1 = '#444444'
         
         # Create and place shapes
-        M = Rectangle(height=d_M[0], width=d_M[1], fill_color=DARKER_GREY, fill_opacity=1, stroke_color=NEON_GREEN)
+        M = Rectangle(height=d_M[0], width=d_M[1], fill_color=GREY_1, fill_opacity=1, stroke_color=NEON_GREEN)
         M.shift(r_M_O[0]*RIGHT+r_M_O[1]*UP)
         
-        w1 = Circle(radius=wheelradius, fill_color=DARKER_GREY, fill_opacity=1, stroke_color=NEON_GREEN)
+        w1 = Circle(radius=wheelradius, fill_color=GREY_1, fill_opacity=1, stroke_color=NEON_GREEN)
         w1.shift(r_w1_O[0]*RIGHT+r_w1_O[1]*UP)
         
-        w2 = Circle(radius=wheelradius, fill_color=DARKER_GREY, fill_opacity=1, stroke_color=NEON_GREEN)
+        w2 = Circle(radius=wheelradius, fill_color=GREY_1, fill_opacity=1, stroke_color=NEON_GREEN)
         w2.shift(r_w2_O[0]*RIGHT+r_w2_O[1]*UP)
         
-        m1 = Circle(radius=m1radius, fill_color=DARKER_GREY, fill_opacity=1, stroke_color=NEON_GREEN)
+        m1 = Circle(radius=m1radius, fill_color=GREY_1, fill_opacity=1, stroke_color=NEON_GREEN)
         m1.shift(r_m1_O[0]*RIGHT+r_m1_O[1]*UP)
         
-        m2 = Circle(radius=m2radius, fill_color=DARKER_GREY, fill_opacity=1, stroke_color=NEON_GREEN)
+        m2 = Circle(radius=m2radius, fill_color=GREY_1, fill_opacity=1, stroke_color=NEON_GREEN)
         m2.shift(r_m2_O[0]*RIGHT+r_m2_O[1]*UP)
+          
+        rod1 = Line(np.array(r_C1_O), np.array(r_m1_O), stroke_color=GREY_1)
+        rod2 = Line(np.array(r_C2_O), np.array(r_m2_O), stroke_color=GREY_1)
         
-        # Massless rods connecting the cart and pendulum        
-        rod1 = Line(np.array(r_C1_O), np.array(r_m1_O), stroke_color=DARKER_GREY)
-        rod2 = Line(np.array(r_C2_O), np.array(r_m2_O), stroke_color=DARKER_GREY)
         
+        # Create groups to move together
+        cart = VGroup(M, w1, w2)
+        p1 = VGroup(m1, rod1)
+        p2 = VGroup(m2, rod2)
+        # Add elements to scene
+        self.add(cart, p1, p2)
+        self.bring_to_back(rod1, rod2)
+        self.always_continually_update = True
         
-        self.play(FadeIn(M), FadeIn(w1), FadeIn(w2))
-        self.bring_to_front(M,m1,m2)
-        self.play(FadeIn(m1), FadeIn(m2), FadeIn(rod1), FadeIn(rod2))
-        
+        movement = 0.2*DOWN
+        self.play(ApplyMethod(cart.move_to, self.relVec(cart, movement)), ApplyMethod(p1.move_to, self.relVec(p1, movement)), ApplyMethod(p2.move_to, self.relVec(p2, movement)))
+#        self.play(FadeIn(m1), FadeIn(m2), FadeIn(rod1), FadeIn(rod2))
+        self.bring_to_back(rod1, rod2)
         self.wait(2)
+        
+        
+    def relVec(self, obj, pos):
+        # generate the vector to move an object to a new position
+        objpos = [obj.get_x(), obj.get_y(), obj.get_z()]
+        return pos+objpos
+            
