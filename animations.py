d3dd146299fc8b972d3825bfb8e9deea0377a572
Corey Spohn <cas584@cornell.edu>
2019-05-19 00:35:56 -0400

    Controls Animation

--- animations.py
+++ animations.py
@@ -196,20 +196,194 @@ class Dynamics(Scene):
 class Controls(Scene):
     def construct(self):
         self.stateSpace()
+        self.controllability()
         
     def stateSpace(self):
-        xddL = TexMobject(r"\ddot{x} = \frac{g m_{1} \theta_{1} - g m_{2} \theta_{2} + u}{M}")
-        t1ddL  = TexMobject(r"\ddot{\theta_1} = \frac{- g m_{2} \theta_{2} + g \left(M + m_{1}\right) \theta_{1} + u}{M l_{1}}")
-        t2ddL = TexMobject(r"\ddot{\theta_2} = \frac{- g m_{1} \theta_{1} + g \left(M + m_{2}\right) \theta_{2} - u}{M l_{2}}")
+        xddL = TexMobject(r"\ddot{x} \,", r"= \frac{g m_{1} \theta_{1} - g m_{2} \theta_{2} + u}{M}")
+        t1ddL  = TexMobject(r"\ddot{\theta_1} \,", r" = \frac{- g m_{2} \theta_{2} + g \left(M + m_{1}\right) \theta_{1} + u}{M l_{1}}")
+        t2ddL = TexMobject(r"\ddot{\theta_2} \,", r" = \frac{- g m_{1} \theta_{1} + g \left(M + m_{2}\right) \theta_{2} - u}{M l_{2}}")
+        
         
         xddL.to_edge(UP)
         t1ddL.next_to(xddL, DOWN)
         t2ddL.next_to(t1ddL, DOWN)
         
+        for var in [xddL, t1ddL, t2ddL]:
+            var[0].scale(1.75)
         
+        self.add(xddL, t1ddL, t2ddL)
+#        self.play(
+#                 xddL[0].scale, 2,
+#                 run_time=1,)
+        self.play(
+                xddL.scale, 0.55,
+                xddL.to_corner, UP+LEFT,
+                t1ddL.scale, 0.55,
+                t1ddL.to_edge, UP,
+                t2ddL.scale, 0.55,
+                t2ddL.to_corner, UP+RIGHT,
+                run_time=1,)
+        
+        self.wait(1)
+        # State vector
+        z = TexMobject(r"z = ", r"\left[\begin{matrix} x \\ \dot{x} \\ \theta_1 \\ \dot{\theta_1} \\ \theta_2 \\ \dot{\theta_2} \end{matrix}\right]")
+        zd = TexMobject(r"\dot{z} =",  r"\left[\begin{matrix} \dot{x} \\ \ddot{x} \\ \dot{\theta_1} \\ \ddot{\theta_1} \\ \dot{\theta_2} \\ \ddot{\theta_2} \end{matrix}\right]")
+        zvec = z[1].copy()
+        
+        zdexpr = TexMobject(r"\dot{z}", r" =", r"A", r"z", r"+", r"B", r'u')
+        deriv = TexMobject(r"\frac{\partial}{\partial t}")
+        
+        deriv.next_to(z, LEFT)
+        self.play(Write(z))
+        self.wait(2)
+        self.play(Write(deriv))
+        self.wait(2)
+        self.play(
+                ReplacementTransform(z, zd),
+                deriv.fade,1)
+        self.wait(2)
+        self.play(
+                zd[1].to_edge, LEFT,
+                zd[0].fade, 1,
+                run_time=1)
+        self.wait(2)
+        self.play(
+                Write(zdexpr),
+                run_time=1,)
+        self.wait(2)
+        self.play(
+                zdexpr.next_to, t1ddL, DOWN,
+                run_time=1)
+        self.wait(2)
+        # Write out A and B
+        A = TexMobject(r"= ", r"\left[\begin{matrix}0 & 1 & 0 & 0 & 0 & 0\\0 & 0 & \frac{g m_{1}}{M} & 0 & - \frac{g m_{2}}{M} & 0\\0 & 0 & 0 & 1 & 0 & 0\\0 & 0 & \frac{g \left(M + m_{1}\right)}{M l_{1}} & 0 & - \frac{g m_{2}}{M l_{1}} & 0\\0 & 0 & 0 & 0 & 0 & 1\\0 & 0 & - \frac{g m_{1}}{M l_{2}} & 0 & \frac{g \left(M + m_{2}\right)}{M l_{2}} & 0\end{matrix}\right]")
+        
+        A.next_to(zd[1], RIGHT)
+        zvec.next_to(A, RIGHT)
+        self.play(
+                TransformFromCopy(zdexpr[2], A),
+                TransformFromCopy(zdexpr[3], zvec),
+                run_time=1)
+        self.wait(2)
+        B = TexMobject(r"B = ", r"\left[\begin{matrix}0\\\frac{1}{M}\\0\\\frac{1}{M l_{1}}\\0\\- \frac{1}{M l_{2}}\end{matrix}\right]")
+        plus = TexMobject(r"+")
+        u = TexMobject(r"u")
+        plus.next_to(zvec, RIGHT)
+        B[1].next_to(plus, RIGHT)
+        u.next_to(B, RIGHT)
+        # Full zdot = Az+Bu expression
+        self.play(
+                TransformFromCopy(zdexpr[4], plus),
+                TransformFromCopy(zdexpr[5], B[1]),
+                TransformFromCopy(zdexpr[6], u),
+                run_time=1)
+        
+        self.wait(2)
+        # Clear top of screen
+        zdfullexpr = VGroup(zd[1], A, zvec, plus, B[1], u)
+        self.play(
+                FadeOutAndShift(xddL, UP),
+                FadeOutAndShift(t1ddL, UP),
+                FadeOutAndShift(t2ddL, UP),
+                FadeOutAndShift(zdexpr, UP),
+                zdfullexpr.to_edge, UP,
+                run_time=1)
+        
+        self.wait(2)
+
+        
+        # Now for y = C z
+        yexpr = TexMobject(r"y", r"=", r"C", r"z")
+        yexpr.to_edge(DOWN)
+        yexpr.shift(0.75*UP)
+#        yexpr.align_to(zd[1], LEFT)
+        self.play(Write(yexpr))
+        self.wait(2)
+        # Reduce the zd = Az+Bu expresion to drive home what z is
+        zdsym = TexMobject(r'\dot{z}')
+        zsym = TexMobject(r'z')
+        
+        zdsym.move_to(zd[1])
+        zsym.move_to(zvec)
+        self.play(
+                ReplacementTransform(zd[1],zdsym),
+                ReplacementTransform(zvec, zsym),
+                plus.next_to, zsym, RIGHT,
+                MaintainPositionRelativeTo(B[1], plus),
+                MaintainPositionRelativeTo(u, plus))
+        # Collapse equation
+        self.play(
+                zdsym.next_to, A, LEFT,
+                zsym.next_to, A, RIGHT,
+                MaintainPositionRelativeTo(plus, zsym),
+                MaintainPositionRelativeTo(B[1], zsym),
+                MaintainPositionRelativeTo(u, zsym))
+        self.wait(2)
+        yfull = TexMobject(r"y", r"=", r"\left[\begin{matrix}1 & 0 & 0 & 0 & 0 & 0\\0 & 0 & 1 & 0 & 0 & 0\\0 & 0 & 0 & 0 & 1 & 0\end{matrix}\right]", r'z')
+        yfull.to_edge(DOWN)
+        self.play(
+                ReplacementTransform(yexpr, yfull))
+        self.wait(2)
+        
+        # Set up controllablity section
+        self.play(
+                FadeOutAndShift(zdsym, LEFT),
+                FadeOutAndShift(A[0], LEFT),
+                zsym.fade, 1,
+                FadeOutAndShift(u, RIGHT),
+                FadeOutAndShift(yfull, DOWN),
+                plus.fade, 1,)
+        
+        
+        self.play(
+                A[1].center)
+        self.wait(1)
+        self.play(B[1].next_to, A[1], RIGHT)
+        self.remove(A[1], B[1])
         
     def controllability(self):
-        pass
+        A = TexMobject(r'\left[\begin{matrix}0 & 1 & 0 & 0 & 0 & 0\\0 & 0 & \frac{g m_{1}}{M} & 0 & - \frac{g m_{2}}{M} & 0\\0 & 0 & 0 & 1 & 0 & 0\\0 & 0 & \frac{g \left(M + m_{1}\right)}{M l_{1}} & 0 & - \frac{g m_{2}}{M l_{1}} & 0\\0 & 0 & 0 & 0 & 0 & 1\\0 & 0 & - \frac{g m_{1}}{M l_{2}} & 0 & \frac{g \left(M + m_{2}\right)}{M l_{2}} & 0\end{matrix}\right]')
+        B = TexMobject(r'\left[\begin{matrix}0\\\frac{1}{M}\\0\\\frac{1}{M l_{1}}\\0\\- \frac{1}{M l_{2}}\end{matrix}\right]')
+        A.center
+        B.next_to(A, RIGHT)
+        self.add(A, B)
+        self.play(A.to_edge, UP,
+                  B.to_edge, UP,
+                  )
+        # Controllability matrix
+        Qexpr = TexMobject(r'Q_c', r'=', r'\left[ B \quad A B \quad A^2 B \quad A^3 B \quad A^4 B \quad A^5 B\right]')
+        Qexpr.next_to(A, DOWN)
+        self.play(Write(Qexpr))
+        self.wait(1)
+        
+        self.play(
+                FadeOutAndShift(A, UP),
+                FadeOutAndShift(B, UP),
+                Qexpr.to_edge, UP)
+        self.wait(1)
+#        self.play(Qexpr.stretch, 1.5, 1)
+        
+        # Actual Qc
+        Qc = TexMobject(r'Q_c', r'=', r'\left[\begin{matrix}0 & \frac{1}{M} & 0 & \frac{g \left(l_{1} m_{2} + l_{2} m_{1}\right)}{M^{2} l_{1} l_{2}} & 0 & \frac{g^{2} \left(l_{1} m_{2} \left(l_{1} \left(M + m_{2}\right) + l_{2} m_{1}\right) + l_{2} m_{1} \left(l_{1} m_{2} + l_{2} \left(M + m_{1}\right)\right)\right)}{M^{3} l_{1}^{2} l_{2}^{2}}\\\frac{1}{M} & 0 & \frac{g \left(l_{1} m_{2} + l_{2} m_{1}\right)}{M^{2} l_{1} l_{2}} & 0 & \frac{g^{2} \left(l_{1} m_{2} \left(l_{1} \left(M + m_{2}\right) + l_{2} m_{1}\right) + l_{2} m_{1} \left(l_{1} m_{2} + l_{2} \left(M + m_{1}\right)\right)\right)}{M^{3} l_{1}^{2} l_{2}^{2}} & 0\\0 & \frac{1}{M l_{1}} & 0 & \frac{g \left(l_{1} m_{2} + l_{2} \left(M + m_{1}\right)\right)}{M^{2} l_{1}^{2} l_{2}} & 0 & \frac{g^{2} \left(l_{1} m_{2} \left(l_{1} \left(M + m_{2}\right) + l_{2} \left(M + m_{1}\right)\right) + l_{2} \left(l_{1} m_{1} m_{2} + l_{2} \left(M + m_{1}\right)^{2}\right)\right)}{M^{3} l_{1}^{3} l_{2}^{2}}\\\frac{1}{M l_{1}} & 0 & \frac{g \left(l_{1} m_{2} + l_{2} \left(M + m_{1}\right)\right)}{M^{2} l_{1}^{2} l_{2}} & 0 & \frac{g^{2} \left(l_{1} m_{2} \left(l_{1} \left(M + m_{2}\right) + l_{2} \left(M + m_{1}\right)\right) + l_{2} \left(l_{1} m_{1} m_{2} + l_{2} \left(M + m_{1}\right)^{2}\right)\right)}{M^{3} l_{1}^{3} l_{2}^{2}} & 0\\0 & - \frac{1}{M l_{2}} & 0 & - \frac{g \left(l_{1} \left(M + m_{2}\right) + l_{2} m_{1}\right)}{M^{2} l_{1} l_{2}^{2}} & 0 & - \frac{g^{2} \left(l_{1} \left(l_{1} \left(M + m_{2}\right)^{2} + l_{2} m_{1} m_{2}\right) + l_{2} m_{1} \left(l_{1} \left(M + m_{2}\right) + l_{2} \left(M + m_{1}\right)\right)\right)}{M^{3} l_{1}^{2} l_{2}^{3}}\\- \frac{1}{M l_{2}} & 0 & - \frac{g \left(l_{1} \left(M + m_{2}\right) + l_{2} m_{1}\right)}{M^{2} l_{1} l_{2}^{2}} & 0 & - \frac{g^{2} \left(l_{1} \left(l_{1} \left(M + m_{2}\right)^{2} + l_{2} m_{1} m_{2}\right) + l_{2} m_{1} \left(l_{1} \left(M + m_{2}\right) + l_{2} \left(M + m_{1}\right)\right)\right)}{M^{3} l_{1}^{2} l_{2}^{3}} & 0\end{matrix}\right]')
+        Qc.scale(0.35)
+        Qc.stretch(1.5,1)
+        self.play(Write(Qc))
+        
+        # Determinant of Qc
+        QcBrace = Brace(Qc[2],DOWN)
+        detQc = TexMobject(r'\operatorname{det}Q_c', r'=', r'\frac{g^{6} \left(l_{1} - l_{2}\right)^{2}}{M^{6} l_{1}^{6} l_{2}^{6}}')
+        detQc.next_to(QcBrace, DOWN)
+        self.play(GrowFromCenter(QcBrace), Write(detQc))
+        
+        # Controllability condition
+        noctrl = TexMobject(r'\operatorname{if}\ ', r'l_1', r'=', r'l_2')
+        noctrl.next_to(detQc, LEFT)
+        detQc0 = TexMobject(r'0')
+        detQc0.next_to(detQc[1],RIGHT)
+        self.play(Write(noctrl))
+        
+        self.play(Transform(detQc[2], detQc0))
+        
         
 class CartDemo(Scene):
     def construct(self):
